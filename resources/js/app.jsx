79b686fff64a79f677587d08e14c5838c1cb3075
Eugene Francis Rebong <eugene999.dev@gmail.com>
2025-03-20 15:36:20 +0800

    MUI installed.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,11 @@ import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
