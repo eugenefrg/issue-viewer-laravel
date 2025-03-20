aaac5f00f62e2149fbc0f56489055534029db0c1
Eugene Francis Rebong <eugene999.dev@gmail.com>
2025-03-20 15:56:54 +0800

    initial layout implemented.

--- resources/js/Components/Layout.jsx
+++ resources/js/Components/Layout.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import { Button, Typography } from '@mui/material';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
 export default function Layout({ children }) {
   return (
-    <div>
-          {children}
-    </div>
+    <Container maxWidth="lg"  className='px-4 pt-24'>
+        <AppBar className='py-4'>
+            <Container maxWidth="lg">
+                <Typography variant="h6">Issue Viewer</Typography>
+            </Container>
+        </AppBar>
+        <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+                Issues
+            </Link>
+        </Breadcrumbs>
+      {children}
+    </Container>
   )
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.jsx')
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 </head>
 
 <body>
