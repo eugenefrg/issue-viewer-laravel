1df891f1da1c3d2400e34a101acaa8f6120f1a98
Eugene Francis Rebong <eugene999.dev@gmail.com>
2025-03-21 08:39:15 +0800

    list cosmetics

--- resources/js/Pages/Issue/List.jsx
+++ resources/js/Pages/Issue/List.jsx
@@ -2,40 +2,55 @@ import Layout from '../../Components/Layout';
 import { Head } from '@inertiajs/react';
 import React from 'react';
 import Chip from '../../Components/Chip';
+import { Typography, Divider, Card, CardActionArea } from '@mui/material';
 
 export default function List({ issues }) {
     return (
         <Layout>
             <Head title="List of Issues" />
             {issues.map((issue) => (
-                <div key={issue.id} className="p-4 border-b">
-                    <a
-                        href={`/issue/${issue.repository.owner.login}/${issue.repository.name}/${issue.number}`}
-                    >
-                        <h3 className="font-bold">{issue.title}</h3>
-                        <div className="flex gap-2">
-                            {issue.labels.map((label) => (
-                                <Chip
-                                    key={label.id}
-                                    label={label.name}
-                                    color={label.color}
-                                    className="p-8"
-                                />
-                            ))}
+                <Card
+                    key={issue.id}
+                    className="my-4"
+                    onClick={() => {
+                        window.location.href = `/issue/${issue.repository.owner.login}/${issue.repository.name}/${issue.number}`;
+                    }}
+                >
+                    <CardActionArea>
+                        <div className="p-4 px-4">
+                            <div className="flex items-center gap-2">
+                                <Typography
+                                    variant="h6"
+                                    className="font-bold"
+                                    component="span"
+                                    color="text.secondary"
+                                >
+                                    {issue.repository.owner.login}/{issue.repository.name}
+                                </Typography>
+                                <Typography variant="h6" className="font-bold" component="span">
+                                    {issue.title}
+                                </Typography>
+                            </div>
                             <div className="flex items-center gap-1 text-sm text-gray-600">
                                 <span>#{issue.number}</span>
                                 <span>opened {issue.created_at}</span>
                                 <span>by {issue.user.login}</span>
-                                {issue.pull_request && (
-                                    <span className="text-gray-500">
-                                        • linked to PR #{issue.pull_request.number}
-                                    </span>
-                                )}
                             </div>
+                            <div className="flex gap-2 pt-2 pb-4">
+                                {issue.labels.map((label) => (
+                                    <Chip
+                                        key={label.id}
+                                        label={label.name}
+                                        color={label.color}
+                                        className="p-8"
+                                    />
+                                ))}
+                            </div>
+
+                            <p className="text-gray-600">{issue.body}</p>
                         </div>
-                        <p className="text-gray-600">{issue.body}</p>
-                    </a>
-                </div>
+                    </CardActionArea>
+                </Card>
             ))}
         </Layout>
     );
